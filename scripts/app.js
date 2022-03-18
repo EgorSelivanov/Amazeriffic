c39f760b088df20a2f0500380c732133f8b2f8c1
Beetle <68906257+EgorSelivanov@users.noreply.github.com>
2022-03-18 10:09:50 +0300

    Change app.js, add tab switching

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
-var main = function() {
+var main = function () {
 	"use strict";
-	console.log("hello world!");
+	var tabNumber;
+	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+		$(tabSelector).on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(this).addClass("active");
+		return false;
+		});
+	}
 };
-
-$(document).ready(main);
