4cbe4aadbf9d3fe41b635e4cb29da14770f66c50
Beetle <68906257+EgorSelivanov@users.noreply.github.com>
2022-03-18 12:13:27 +0300

    Add interactivity

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,58 @@
 var main = function () {
 	"use strict";
-	var tabNumber;
-	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(tabSelector).on("click", function () {
-		$(".tabs span").removeClass("active");
-		$(this).addClass("active");
-		return false;
+	var toDos = [
+	"Закончить писать эту книгу",
+	"Вывести Грейси на прогулку в парк", 
+	"Ответить на электронные письма", 
+	"Подготовиться к лекции в понедельник", 
+	"Обновить несколько новых задач", 
+	"Купить продукты"
+	];
+	$(".tabs a span").toArray().forEach(function (element) {
+		//создаем обработку щелчков для этого элемента
+		$(element).on("click", function () {
+			//поскольку мы используем версию элемента jQuery,
+			//нужно создать временную переменную,
+			//чтобы избежать постоянного обновления
+			var $element = $(element);
+			//делаем элементы неактивными
+			$(".tabs a span").removeClass("active");
+			//делаем активным выбранный элемент
+			$element.addClass("active");
+			//очищаем основное содержание, чтобы переопределить его
+			$("main .content").empty();
+			//проверка, является ли предок элемента
+			//первым потомком своего собственного предка
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("Щелчок на первой вкладке!");
+				for (var i = toDos.length-1; i > -1; i--) { 
+					$(".content").append($("<li>").text(toDos[i]));
+				}
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Щелчок на второй вкладке!");
+				toDos.forEach(function (todo) { 
+					$(".content").append($("<li>").text(todo));
+				});
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Щелчок на третьей вкладке!");
+				$(".content").append(
+					'<input type="text" class="input-task">'+
+					'<button class="add-task-btn">Добавить</button>'
+				);
+				var newTask;
+				$('.add-task-btn').on('click',function(){
+					newTask = $('.input-task').val();
+					if (newTask != '') {
+						toDos.push( newTask);
+						alert('Новое задание "' + newTask + '" успешно добавлено!');
+						$('.input-task').val("");
+					}
+				})
+			}
+			return false;
 		});
-	}
+	});
+	$(".tabs a:first-child span").trigger("click");
 };
+$(document).ready(main);
+
